db2aafc465afaa35e2e2363abaf994eb73cfd346
Nilesh <nilesh_wanare@outlook.com>
2026-06-27 22:58:03 +0530

    node_group.tf updated

--- terra-config/node_group.tf
+++ terra-config/node_group.tf
@@ -30,7 +30,7 @@ resource "aws_eks_node_group" "example" {
 resource "aws_launch_template" "eks_node_launch_template" {
   name = "${aws_eks_cluster.eks_cluster.name}-node-template" 
 
-  instance_type = "t2.medium"
+   instance_type = "t3.micro"
 
   metadata_options {
     http_endpoint             = "enabled"
