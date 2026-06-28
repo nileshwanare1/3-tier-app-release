b675c686368841acd0df09c5758623eb50876a0f
Nilesh <nilesh_wanare@outlook.com>
2026-06-28 13:34:32 +0530

    node-group.tf updated

--- terra-config/node_group.tf
+++ terra-config/node_group.tf
@@ -5,10 +5,10 @@ resource "aws_eks_node_group" "example" {
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
+  desired_size = 2
+  min_size     = 2
+  max_size     = 2
+}
 
   update_config {
     max_unavailable = 1
